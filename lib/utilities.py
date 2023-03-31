fa1c266daced3427b7a22cd758df87511e9e8cec
Etherion <19214485+jjziets@users.noreply.github.com>
2023-03-31 22:25:08 +0200

    Update utilities.py

--- lib/utilities.py
+++ lib/utilities.py
@@ -1,6 +1,16 @@
 import requests
 import os
+import time
 
 def telegram_request(path):
-    response = requests.get("https://api.telegram.org/bot" + os.getenv('TELEGRAM_TOKEN') + path)
-    return response.json()
+    while True:
+        response = requests.get("https://api.telegram.org/bot" + os.getenv('TELEGRAM_TOKEN') + path)
+        response_json = response.json()
+
+        # Check if the response contains a rate limit error (error code 429)
+        if response.status_code == 429:
+            retry_after = response_json.get('parameters', {}).get('retry_after', 0)
+            print(f"Rate limit hit. Retrying after {retry_after + 1}s...")
+            time.sleep(retry_after + 1)
+        else:
+            return response_json
