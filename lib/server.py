192e40d50df9addbd179e587f7e5d2a6c4d6f196
Etherion <19214485+jjziets@users.noreply.github.com>
2023-03-31 22:32:42 +0200

    Update server.py

--- lib/server.py
+++ lib/server.py
@@ -1,15 +1,32 @@
 from flask import jsonify, request, Flask
-from threading import Timer
+from threading import Timer, Event
 import os
 from utilities import telegram_request
 app = Flask(__name__)
 
 timers = {}
+pause_events = {}
 
 def missed_ping(worker):
+    pause_event = pause_events.get(worker)
+    if pause_event is not None:
+        pause_event.wait()
+
     del timers[worker]
     print("Missed ping for", worker)
-    telegram_request("/sendMessage?chat_id=" + os.getenv("CHAT_ID") + "&text=" + worker + " is down")
+    response = telegram_request("/sendMessage?chat_id=" + os.getenv("CHAT_ID") + "&text=" + worker + " is down")
+
+    if response.get('error_code') == 429:
+        retry_after = response.get('parameters', {}).get('retry_after', 0) + 1
+        print(f"Pausing all timers for {retry_after}s...")
+        
+        for w, e in pause_events.items():
+            e.clear()
+
+        time.sleep(retry_after)
+
+        for w, e in pause_events.items():
+            e.set()
 
 @app.route('/ping/<worker_id>', methods=['GET'])
 def app_stats(worker_id):
@@ -28,6 +45,9 @@ def app_stats(worker_id):
         telegram_request("/sendMessage?chat_id=" + os.getenv("CHAT_ID") + "&text=" + worker_id + " is up")
 
     print("Creating timer for:", worker_id)
+    pause_event = Event()
+    pause_event.set()
+    pause_events[worker_id] = pause_event
     timers[worker_id] = Timer(int(os.getenv("FAIL_TIMEOUT")), missed_ping, [worker_id])
     timers[worker_id].start()
 
